29cefabb6fdb45f45975e1683f690470b10dd312
billkit <383089046@qq.com>
2024-06-15 02:32:24 +0800

    Create all-cn-rule-v7.sh

---
+++ all-cn-rule-v7.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# 输出文件名
+output_file="/opt/routeros/route-rule/all-cn-router-V7.rsc"
+
+# 清空输出文件
+> "$output_file"
+
+# 删除现有的所有路由规则
+echo ":foreach i in=[/routing rule find ] do=[/routing rule remove \$i]" >> "$output_file"
+
+# 进入路由规则配置模式
+echo "/routing rule" >> "$output_file"
+
+# 读取并添加 中国大陆IP 文件中的网络地址到路由表 CN
+if [ -f /opt/routeros/route-rule/all-cn.txt ]; then
+  while IFS= read -r net; do
+    echo "add dst-address=$net action=lookup table=CN" >> "$output_file"
+  done < /opt/routeros/route-rule/all-cn.txt
+else
+  echo "文件 all-cn.txt 未找到." >> "$output_file"
+fi
+
+
+echo "配置已生成到 $output_file"
