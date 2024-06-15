ea3c89a93882ebe8ccac38bf935183bc35b8687d
billkit <383089046@qq.com>
2024-06-15 12:03:27 +0800

    Update route-rule-v7.sh

--- route-rule-v7.sh
+++ route-rule-v7.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 输出文件名
-output_file="/opt/routeros/route-rule/ros-router-v7.rsc"
+output_file="./route-rule/ros-router-v7.rsc"
 
 # 清空输出文件
 > "$output_file"
@@ -13,28 +13,28 @@ echo ":foreach i in=[/routing rule find ] do=[/routing rule remove \$i]" >> "$ou
 echo "/routing rule" >> "$output_file"
 
 # 读取并添加 中国联通文件中的网络地址到路由表 UNICOM
-if [ -f /opt/routeros/route-rule/unicom.txt ]; then
+if [ -f ./iplist/unicom.txt ]; then
   while IFS= read -r net; do
     echo "add dst-address=$net action=lookup table=UNICOM" >> "$output_file"
-  done < /opt/routeros/route-rule/unicom.txt
+  done < ./iplist/unicom.txt
 else
   echo "文件 unicom.txt 未找到." >> "$output_file"
 fi
 
 # 读取并添加 中国电信 文件中的网络地址到路由表 CT
-if [ -f /opt/routeros/route-rule/chinatelecom.txt ]; then
+if [ -f ./iplist/chinatelecom.txt ]; then
   while IFS= read -r net; do
     echo "add dst-address=$net action=lookup table=CT" >> "$output_file"
-  done < /opt/routeros/route-rule/chinatelecom.txt
+  done < ./iplist/chinatelecom.txt
 else
   echo "文件 chinatelecom.txt 未找到." >> "$output_file"
 fi
 
 # 读取并添加 中国移动 文件中的网络地址到路由表 CMCC
-if [ -f /opt/routeros/route-rule/cmcc.txt ]; then
+if [ -f ./iplist/cmcc.txt ]; then
   while IFS= read -r net; do
     echo "add dst-address=$net action=lookup table=CMCC" >> "$output_file"
-  done < /opt/routeros/route-rule/cmcc.txt
+  done < ./iplist/cmcc.txt
 else
   echo "文件 cmcc.txt 未找到." >> "$output_file"
 fi
