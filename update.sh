255cc66b60b21a249b24e0ec189449e040730331
billkit <39383468+billkit@users.noreply.github.com>
2024-06-15 12:36:49 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@
 wget -O ./iplist/cmcc.txt https://ispip.clang.cn/cmcc_cidr.txt
 wget -O ./iplist/unicom.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
 wget -O ./iplist/chinatelecom.txt https://ispip.clang.cn/chinatelecom_cidr.txt
-wget -O ./iplist/all-cn.txt https://ispip.clang.cn/all_cn_cidr.txt
-wget -O ./iplist/oversea.txt https://raw.githubusercontent.com/billkit/ip-area-route/master/data/apnic_china_0_v4.txt
+wget -O ./iplist/all-cn.txt https://raw.githubusercontent.com/billkit/ip-area-route/master/data/apnic_china_0_v4.txt
+wget -O ./iplist/oversea.txt https://raw.githubusercontent.com/billkit/ip-area-route/master/data/apnic_oversea_0_v4.txt
 
 #rm ./route-rule/*.rsc
 bash all-cn-rule-v6.sh 
