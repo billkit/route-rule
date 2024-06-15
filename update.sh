ae7f10356c74ec37d729d5b826de357ff688076c
billkit <39383468+billkit@users.noreply.github.com>
2024-06-15 10:56:25 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-rm  /opt/routeros/route-rule/*.txt
-wget -O /opt/routeros/route-rule/cmcc.txt https://ispip.clang.cn/cmcc_cidr.txt
-wget -O /opt/routeros/route-rule/unicom.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
-wget -O /opt/routeros/route-rule/chinatelecom.txt https://ispip.clang.cn/chinatelecom_cidr.txt
-wget -O /opt/routeros/route-rule/all-cn.txt https://ispip.clang.cn/all_cn_cidr.txt
+rm  ./iplist/*.txt
+wget -O ./iplist/cmcc.txt https://ispip.clang.cn/cmcc_cidr.txt
+wget -O ./iplist/unicom.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
+wget -O ./iplist/chinatelecom.txt https://ispip.clang.cn/chinatelecom_cidr.txt
+wget -O ./iplist/all-cn.txt https://ispip.clang.cn/all_cn_cidr.txt
+wget -O ./iplist/oversea.txt https://raw.githubusercontent.com/billkit/ip-area-route/master/data/apnic_china_0_v4.txt
 
-rm /opt/routeros/route-rule/*.rsc
+rm ./route-rule/*.rsc
 bash all-cn-rule-v6.sh 
 bash route-rule-v6.sh
 bash all-cn-rule-v7.sh 
 bash route-rule-v7.sh
+bash oversea-rule.sh
