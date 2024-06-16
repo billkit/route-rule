39e7912150f4c7cb9c3d7681d8e348e4475d7ca5
billkit <39383468+billkit@users.noreply.github.com>
2024-06-17 02:40:08 +0800

    Update oversea-v6.sh

--- oversea-v6.sh
+++ oversea-v6.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 输出文件名
-output_file="./route-rule/ros-router-v6.rsc"
+output_file="./route-rule/oversea-router-v6.rsc"
 
 # 清空输出文件
 > "$output_file"
@@ -12,31 +12,14 @@ echo ":foreach i in=[/ip route rule find ] do=[/ip route rule remove \$i]" >> "$
 # 进入路由规则配置模式
 echo "/ip route rule" >> "$output_file"
 
-# 读取并添加 Unicom.TXT 文件中的网络地址到路由表 UNICOM
-if [ -f ./iplist/unicom.txt ]; then
+# 读取并添加oversea.TXT文件中的网络地址到路由表 OVERSEA
+if [ -f ./iplist/oversea.txt ]; then
   while IFS= read -r net; do
-    echo "add dst-address=$net action=lookup table=UNICOM" >> "$output_file"
-  done < ./iplist/unicom.txt
+    echo "add dst-address=$net action=lookup table=OVERSEA" >> "$output_file"
+  done < ./iplist/oversea.txt
 else
-  echo "文件 unicom.txt 未找到." >> "$output_file"
+  echo "文件 oversea.txt 未找到." >> "$output_file"
 fi
 
-# 读取并添加 chinatelecom.txt 文件中的网络地址到路由表 CT
-if [ -f ./iplist/chinatelecom.txt ]; then
-  while IFS= read -r net; do
-    echo "add dst-address=$net action=lookup table=CT" >> "$output_file"
-  done < ./iplist/chinatelecom.txt
-else
-  echo "文件 chinatelecom.txt 未找到." >> "$output_file"
-fi
-
-# 读取并添加 CMCC.TXT 文件中的网络地址到路由表 CMCC
-if [ -f ./iplist/cmcc.txt ]; then
-  while IFS= read -r net; do
-    echo "add dst-address=$net action=lookup table=CMCC" >> "$output_file"
-  done < ./iplist/cmcc.txt
-else
-  echo "文件 cmcc.txt 未找到." >> "$output_file"
-fi
 
 echo "配置已生成到 $output_file"
